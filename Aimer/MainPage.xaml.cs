f9b6412d21656e4d3714bc9721fc9d1f444103f1
DELL <DELL@DESKTOP-DLGPSE4>
2017-04-17 14:04:06 +0800

    default ip

--- Aimer/MainPage.xaml.cs
+++ Aimer/MainPage.xaml.cs
@@ -33,6 +33,10 @@ namespace Aimer
         public MainPage()
         {
             this.InitializeComponent();
+            this.Loaded += (s, e) =>
+             {
+                 frame.Navigate(typeof(WebPage));
+             };
         }
 
         private async void red_Click(object sender, RoutedEventArgs e)
@@ -45,10 +49,6 @@ namespace Aimer
             await writer.WriteLineAsync(request);
             await writer.FlushAsync();
 
-            //Read data from the echo server.
-            Stream streamIn = socket.InputStream.AsStreamForRead();
-            StreamReader reader = new StreamReader(streamIn);
-            string response = await reader.ReadLineAsync();
         }
 
         private async void blue_Click(object sender, RoutedEventArgs e)
@@ -61,10 +61,6 @@ namespace Aimer
             await writer.WriteLineAsync(request);
             await writer.FlushAsync();
 
-            //Read data from the echo server.
-            Stream streamIn = socket.InputStream.AsStreamForRead();
-            StreamReader reader = new StreamReader(streamIn);
-            string response = await reader.ReadLineAsync();
         }
 
         private async void green_Click(object sender, RoutedEventArgs e)
@@ -77,10 +73,6 @@ namespace Aimer
             await writer.WriteLineAsync(request);
             await writer.FlushAsync();
 
-            //Read data from the echo server.
-            Stream streamIn = socket.InputStream.AsStreamForRead();
-            StreamReader reader = new StreamReader(streamIn);
-            string response = await reader.ReadLineAsync();
         }
         
         private async void connectbutton_ClickAsync(object sender, RoutedEventArgs e)
@@ -90,8 +82,6 @@ namespace Aimer
             
             try
             {
-                Windows.Networking.Sockets.StreamSocket socket = new Windows.Networking.Sockets.StreamSocket();
-
                 Windows.Networking.HostName serverHost = new Windows.Networking.HostName(Ip);
 
                 //Every protocol typically has a standard port number. For example HTTP is typically 80, FTP is 20 and 21, etc.
@@ -103,7 +93,7 @@ namespace Aimer
             {
 
             }
-
+            ConnectSucess.Visibility = Visibility;
 
             blue.IsEnabled = true;
             red.IsEnabled = true;
@@ -111,87 +101,7 @@ namespace Aimer
            
         }
 
-        //private  void ScenButton_Click(object sender, RoutedEventArgs e)
-        //{
-        //    Button Btn = (Button)sender;
-
-        //    var BtnName=Btn.Name.Remove(0, 6);
-
-        //    ImgButtonPanel.Margin = new Thickness(96, 192, 192, 96);
-
-        //    for (int i = 0; i < data.scenslist.Count; i++)
-        //    {
-
-        //        if (data.scenslist.Keys.ElementAt(i).ToString()== BtnName)
-        //        {
-        //            var key = data.scenslist.Keys.ElementAt(i);
-        //            var value= data.scenslist[key];
-        //            var Path1 = value.imgpath1;
-        //            var Path2 = value.imgpath2;
-        //            var Path3 = value.imgpath3;
-        //            Image img1 = new Image();
-        //            Image img2 = new Image();
-        //            Image img3 = new Image();
-        //            BitmapImage bitmap1 = new BitmapImage();
-        //            bitmap1.UriSource = new Uri(Path1);
-        //            BitmapImage bitmap2 = new BitmapImage();
-        //            bitmap2.UriSource = new Uri(Path2);
-        //            BitmapImage bitmap3 = new BitmapImage();
-        //            bitmap3.UriSource = new Uri(Path3);
-        //            img1.Source = bitmap1;
-        //            img2.Source = bitmap2;
-        //            img3.Source = bitmap3;
-        //            ImgButtonPanel.Children.Add(img1);
-        //            ImgButtonPanel.Children.Add(img2);
-        //            ImgButtonPanel.Children.Add(img3);
-
-        //        }
-
-
-        //    }
-
-
-
-
-
-        //}
-        //ScenResponse data;
-        //GoodSceneResponse data2;
-        //private async void Button_Click(object sender, RoutedEventArgs e)
-        //{
-        //    FittingRoomClient client = new FittingRoomClient();
-        //    Button.Content = "Waiting";
-        //    data = await client.GetScenesAsyc();
-        //    Button.Content = data.response;
-
-
-
-        //    for (int i = 0; i < data.scenslist.Count; i++)
-        //    {
-        //        var scene = data.scenslist.ElementAt(i);
-        //        var key = scene.Key;
-        //        var value = scene.Value;
-        //        Button btn = new Button();
-        //        btn.Width = 110;
-        //        btn.Height = 110;
-        //        btn.Content = value.enscene+ "\r\n" + value.flag;
-        //        btn.Name = "Button" + i;
-        //        scenButtonPanel.Children.Add(btn);
-        //        btn.Click += ScenButton_Click;
-
-        //    }
-
-
-        //}
-
-
-        //private async void ButtonTwo_Click(object sender, RoutedEventArgs e)
-        //{
-        //    FittingRoomClient client = new FittingRoomClient();
-        //    data2 = await client.GetGoodsScenesAsyc();
-        //    Button.Content = data2.response;
-        //}
-
+       
 
     }
 }
